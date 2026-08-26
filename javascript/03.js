d790bfda1071d6992dd6c58a720980e5696868b1
Nixon Oliviera Marinho <nixon.projetos@gmail.com>
2026-08-26 11:39:21 -0400

    Exercícios Javascript

---
+++ javascript/03.js
@@ -0,0 +1,16 @@
+// Operadores Aritméticos
+
+let n1 = 200
+let n2 = 5
+
+let adicao = n1 + n2
+let subtracao = n1 - n2
+let multiplicacao = n1 * n2
+let divisao = n1 / n2
+let resto = n1 % n2
+
+console.log(`${n1} + ${n2} = ${adicao}`)
+console.log(`${n1} - ${n2} = ${subtracao}`)
+console.log(`${n1} * ${n2} = ${multiplicacao}`)
+console.log(`${n1} / ${n2} = ${divisao}`)
+console.log(`${n1} % ${n2} = ${resto}`)
---
+++ javascript/04.js
@@ -0,0 +1,5 @@
+// Exercícios:
+
+// *Uma loja vende um produto por R$ 45,00. Um cliente deseja comprar 6 unidades.
+// *Crie um programa que calcule o valor total da compra utilizando o operador *
+
