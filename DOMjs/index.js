667d3a873ff64aaf581fd18058b73f4e71f7abc2
Nixon Oliviera Marinho <nixon.projetos@gmail.com>
2026-08-28 11:42:21 -0400

    DOM

---
+++ DOMjs/index.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("quadrado")[0].style.backgroundColor = "red"
+document.getElementsByClassName("quadrado")[0].style.color = "yellow"
+document.getElementsByClassName("quadrado")[0].style.fontSize = "36px";
+document.getElementsByClassName("quadrado")[0].style.textTransform = "uppercase"
+document.getElementsByClassName("quadrado")[0].innerHTML="Nova mensagem"
+
