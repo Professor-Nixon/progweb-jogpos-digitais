8561336e13b3dd2ed92395025f1e5fec4481a9ed
Nixon Oliviera Marinho <nixon.projetos@gmail.com>
2026-08-31 09:35:27 -0400

    DOM 02

---
+++ DOMjs/DOM02/index.js
@@ -0,0 +1,4 @@
+document.getElementById("formas").style.backgroundColor = "red"
+document.getElementById("formas").style.color = "white"
+document.getElementById("formas").innerHTML = "Círculo"
+document.getElementById("formas").style.borderRadius="50%"
